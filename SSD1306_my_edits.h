b9238e9d85dfad06b38628f916abcb1d018380b3
ryan kalysten <rcjk@Bane.lan>
2022-02-07 20:55:39 -0800

    Comments to clarify

--- SSD1306_my_edits.h
+++ SSD1306_my_edits.h
@@ -25,6 +25,29 @@
 *******************************************************************************/
 
 
+// ============================ GDDRAM Structure =========================== //
+/*            No row re-mapping and column re-mapping
+ *
+ *     PAGE0 (COM0-COM7)   |________PAGE 0_________|
+ *     PAGE0 (COM8-COM15)  |________PAGE 1_________|
+ *     PAGE0 (COM16-COM23) |________PAGE 2_________|
+ *     PAGE0 (COM24-COM31) |________PAGE 3_________|
+ *
+ *     S S S S
+ *     E E E E
+ *     G G G G
+ *     0 1.. 127
+ *     _ _ _ _
+ *    |_|_|_|_| LSB D[0]
+ *    |_|_|_|_|
+ *    |_|_|_|_|             <----- One Page
+ *    |_|_|_|_|
+ *    |_|_|_|_|
+ *    |_|_|_|_|
+ *    |_|_|_|_|
+ *    |_|_|_|_| MSB D[7]
+ *     */
+
 
 #include <stdint.h>
 #include <stdbool.h>
@@ -59,9 +82,9 @@
 #define MEMORYMODE           0x20
 #define COLUMNADDR           0x21
 #define PAGEADDR             0x22
-#define COMSCANINC           0xC0
-#define COMSCANDEC           0xC8
-#define SEGREMAP             0xA0
+#define COMSCANCOM0          0xC0
+#define COMSCANCOM_N         0xC8 // Scan from COM[N-1] to COM0 where N is multuplixed ratio
+#define SEGREMAP             0xA1
 #define CHARGEPUMP           0x8D
 #define EXTERNALVCC          0x01
 #define SWITCHCAPVCC         0x02
@@ -78,9 +101,11 @@
 /* Global Variables */
 uint8_t _vccstate, x_pos = 1, y_pos = 1;
 
-//--------------------------------------------------------------------------//
 
 bool wrap = true;
+
+//--------------------------------------------------------------------------//
+
 
 const char Font[] = {
 0x00, 0x00, 0x00, 0x00, 0x00,
@@ -190,52 +215,59 @@ void I2C_Command(uint8_t data){
     I2C_Write(I2C_ADDRESS); // Write address
     I2C_Write(control); // Write command mode
     I2C_Write(data); // Command
-    I2C_Stop();
+    I2C_Stop();     // Stop Bit
     
     
     
 }
 
 void SSD1306_Init(){
-    I2C_Command(DISPLAYOFF);
-    I2C_Command(SETMULTIPLEX);
-    I2C_Command(0x3F);
-    I2C_Command(SETDISPLAYOFFSET);
-    I2C_Command(0x00);
-    I2C_Command(SETSTARTLINE);
-    I2C_Command(SEGREMAP);
-    I2C_Command(0xA1);
-    I2C_Command(COMSCANINC);
-    I2C_Command(SETCOMPINS);
-    I2C_Command(2);
-    I2C_Command(SETCONTRAST);
-    I2C_Command(0x7F);
-    I2C_Command(DISPLAYALLON);
-    I2C_Command(NORMALDISPLAY);
-    I2C_Command(SETDISPLAYCLOCKDIV);
-    I2C_Command(0x80);
-    I2C_Command(CHARGEPUMP);
-    I2C_Command(0x14);
-    I2C_Command(DISPLAYON); 
+    I2C_Command(DISPLAYOFF);     // 0xAE
+    I2C_Command(SETMULTIPLEX);  // 0xA8
+    I2C_Command(0x1F);     // Sets MUX to 31 for 128x32 OLED. This means PAGE0 to PAGE3
+                           // PAGE0 (COM0-COM7)
+                           // PAGE1 (COM8-COM15)
+                           // PAGE2 (COM16-COM23)
+                           // PAGE3 (COM24-COM31)
+    I2C_Command(SETDISPLAYOFFSET);     // 0xD3 Sets display RAM start line 0 - 63 with 0b00000
+                                       // Example to move COM 16 to COM 0, 0b10000 || 0x10
+    I2C_Command(0x00); //Offset is COM0
+    I2C_Command(SETSTARTLINE);     //Sets display RAM start line register from 0-63
+                                   // opcode is 0x40 to 0x7F which looks like 0b01XXXXX test
+    I2C_Command(SEGREMAP);     // 0xA0 Wll map SEG0 (RESET) to either column address 0 (0xA0) or column address 127 (0xA1)
+    I2C_Command(COMSCANCOM0);  // 0xC0 normal mode scan from COM0 to COM[N-1]
+    I2C_Command(SETCOMPINS);  // 0xDA referenced page 44 in datasheet. opcode is 0b00XX0010. A[4]=0b sequential com pin configuration ~ A[4]=1b alernative come pin
+                              // A[5]=0b Disable COM left/right remap ~ A[5]=1b Enable COM left/right remap
+    I2C_Command(0x22);        // A[4]=0 A[5]=1
+    I2C_Command(SETCONTRAST); // 0x81 double byte command, select 1 out of 256. 0x7F resets
+    I2C_Command(0x7F);        // reset contrast
+    I2C_Command(DISPLAYALLON); // 0xA5 Entire display ON output ignores the RAM content
+    I2C_Command(NORMALDISPLAY); // 0xA6 for normal or 0xA7 for reverse. In normal, RAM data of 1 indicates an "ON" pixel while reverse a 0 indicates an "OFF" pixel
+    I2C_Command(SETDISPLAYCLOCKDIV); // 0xD5 Display Clock Divide Ratio A[3:0] divide ration is 1 to 16.
+                                     // Oscillator frequency A[7:4] defualt value is 1000b.
+    I2C_Command(0x80);        // dafualt value ~380kHz
+    I2C_Command(CHARGEPUMP); // 0x8D A[2]=0b disbale charge pump A[2]=1b enable charge pump opcode 010X00b
+    I2C_Command(0x14);        //enable charge pump
+    I2C_Command(DISPLAYON);   //Display ON normal mode
     
     
 }
 
 void ClearDisplay(){
     
-    I2C_Command(COLUMNADDR);
-    I2C_Command(0);
-    I2C_Command(PAGEADDR);
-    I2C_Command(0);
-    I2C_Command(3);
+    I2C_Command(COLUMNADDR); // set start column
+    I2C_Command(0);          // set SEG0
+    I2C_Command(PAGEADDR);   // Set start row and end row 3bits A[2:0] range 0-7d B[2:0] 0-7d
+    I2C_Command(0);          // PAGE0
+    I2C_Command(3);          // End at PAGE3 for 32 row OLED
     
-    I2C_Start();
-    I2C_Write(I2C_ADDRESS);
-    I2C_Write(0x40);
-    
-    for(uint16_t i = 0; i < LCDHEIGHT * LCDWIDTH / 8; i++)
-        I2C_Write(0);
-    I2C_Stop();
+    I2C_Start();          // Start bit
+    I2C_Write(I2C_ADDRESS);   // write address of device
+    I2C_Write(SETSTARTLINE);  // 0x40 Set display RAM start line register. Opcode 01xxxxxx range 0x40 ~ 7F.
+
+    for(uint16_t i = 0; i < LCDHEIGHT * LCDWIDTH / 8; i++) // assign i as unisgned 16 and if it is less then ((128 *32)/8)= 512
+        I2C_Write(0); // write 0 to clear display
+    I2C_Stop(); // Sopt bit
                
 }
 
